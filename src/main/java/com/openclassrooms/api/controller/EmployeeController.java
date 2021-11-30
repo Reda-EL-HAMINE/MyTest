edde723501b64ae8f0d88c346bff3b53b8ea9dd1
Reda-EL-HAMINE <elhareda1@gmail.com>
2021-11-30 09:22:41 +0100

    first commit

---
+++ src/main/java/com/openclassrooms/api/controller/EmployeeController.java
@@ -0,0 +1,31 @@
+package com.openclassrooms.api.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.openclassrooms.api.model.Employee;
+import com.openclassrooms.api.service.EmployeeService;
+
+@RestController
+public class EmployeeController {
+	@Autowired
+    private EmployeeService employeeService;
+
+    /**
+    * Read - Get all employees
+    * @return - An Iterable object of Employee full filled
+    */
+    @GetMapping("/employees")
+    public Iterable<Employee> getEmployees() {
+        return employeeService.getEmployees();
+    }
+    
+    @GetMapping(path="/employee")
+    public Optional<Employee> getEmployee(@RequestParam Long id) {
+        return employeeService.getEmployee(id);
+    }
+}
